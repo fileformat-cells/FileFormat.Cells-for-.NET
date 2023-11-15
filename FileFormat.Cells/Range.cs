91fe4295173294379f87970099d7f1a6b615dc02
Fahad Adeel <>
2023-11-15 16:36:20 +0500

    Implemented new features and enhancements in the Range class
    - Added ClearRange and MergeCells methods to the Range class, enabling the clearing of cell contents and merging of cells within specified ranges.
    - Implemented ColumnCount and RowCount properties in the Range class to provide easy access to the number of columns and rows in a given range.
    - Developed the AddDropdownListValidation method in the Worksheet class for adding dropdown list data validation to cells.
    - Introduced ColumnWidth and RowHeight methods to retrieve the width of a column and the height of a row, enhancing cell dimension handling capabilities.
    
    These enhancements aim to improve the flexibility and utility of the Range class, offering more advanced functionalities for manipulating and validating spreadsheet data.

--- FileFormat.Cells/Range.cs
+++ FileFormat.Cells/Range.cs
@@ -15,6 +15,19 @@ namespace FileFormat.Cells
         public uint EndRowIndex { get; }
         public uint EndColumnIndex { get; }
 
+        // Returns the count of columns in the range
+        public uint ColumnCount
+        {
+            get { return EndColumnIndex - StartColumnIndex + 1; }
+        }
+
+        // Returns the count of rows in the range
+        public uint RowCount
+        {
+            get { return EndRowIndex - StartRowIndex + 1; }
+        }
+
+        
         public Range(Worksheet worksheet, uint startRowIndex, uint startColumnIndex, uint endRowIndex, uint endColumnIndex)
         {
             _worksheet = worksheet ?? throw new ArgumentNullException(nameof(worksheet));
@@ -36,6 +49,48 @@ namespace FileFormat.Cells
                 }
             }
         }
+
+        public void ClearCells()
+        {
+            for (uint row = StartRowIndex; row <= EndRowIndex; row++)
+            {
+                for (uint column = StartColumnIndex; column <= EndColumnIndex; column++)
+                {
+                    var cellReference = $"{ColumnIndexToLetter(column)}{row}";
+                    var cell = _worksheet.GetCell(cellReference);
+                    if (cell != null)
+                    {
+                        cell.PutValue(string.Empty); // Clearing the value
+                        cell.ApplyStyle(0); // Resetting the style if needed
+                    }
+                }
+            }
+        }
+
+        public void MergeCells()
+        {
+            string startCellReference = $"{ColumnIndexToLetter(StartColumnIndex)}{StartRowIndex}";
+            string endCellReference = $"{ColumnIndexToLetter(EndColumnIndex)}{EndRowIndex}";
+
+            _worksheet.MergeCells(startCellReference, endCellReference);
+        }
+
+
+        public void AddDropdownListValidation(string[] options)
+        {
+            
+
+            for (uint row = StartRowIndex; row <= EndRowIndex; row++)
+            {
+                for (uint column = StartColumnIndex; column <= EndColumnIndex; column++)
+                {
+                    var cellReference = $"{ColumnIndexToLetter(column)}{row}";
+                    _worksheet.AddDropdownListValidation(cellReference, options);
+                }
+            }
+        }
+
+
 
         private static string ColumnIndexToLetter(uint columnIndex)
         {
